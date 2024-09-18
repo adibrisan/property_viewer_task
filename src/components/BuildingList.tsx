ef84c4d7fad1b447193745447556ea036345da82
adibrisan <adi.brisan@yahoo.com>
2024-09-18 12:59:41 +0300

    add icon to edit the building from the list

--- src/components/BuildingList.tsx
+++ src/components/BuildingList.tsx
@@ -1,7 +1,10 @@
-import { List } from 'antd';
+import { Row, Col, List, Typography, Space } from 'antd';
 import { useSelector } from 'react-redux';
+import { EditOutlined } from '@ant-design/icons';
 
 import { RootState } from '../store';
+
+const { Text } = Typography;
 
 const BuildingList = () => {
   const buildings = useSelector(
@@ -12,16 +15,29 @@ const BuildingList = () => {
     <List
       dataSource={buildings}
       renderItem={building => (
-        <List.Item>
-          <List.Item.Meta
-            title={building.name}
-            description={`${building.street}, ${building.number}, ${building.city}, ${building.country}`}
-          />
-          <div>
-            Coordinates: {building.coordinates.latitude},{' '}
-            {building.coordinates.longitude}
-          </div>
-          <div>Price: {building.price}</div>
+        <List.Item
+          style={{ padding: '20px' }}
+          extra={
+            <EditOutlined style={{ fontSize: '26px', cursor: 'pointer' }} />
+          }
+        >
+          <Row style={{ width: '100%' }} align="middle">
+            <Col span={12}>
+              <List.Item.Meta
+                title={building.name}
+                description={`${building.street}, ${building.number}, ${building.city}, ${building.country}`}
+              />
+            </Col>
+            <Col span={12}>
+              <Space>
+                <Text italic>
+                  Coordinates: lat: {building.coordinates.latitude.toFixed(3)},
+                  lng: {building.coordinates.longitude.toFixed(3)}
+                </Text>
+                <Text>Price: {building.price}</Text>
+              </Space>
+            </Col>
+          </Row>
         </List.Item>
       )}
     />
