546654e2f248063fed15b639021caa06d9765afc
adibrisan <adi.brisan@yahoo.com>
2024-09-18 13:18:37 +0300

    changed building list style

--- src/components/BuildingList.tsx
+++ src/components/BuildingList.tsx
@@ -1,4 +1,4 @@
-import { Row, Col, List, Typography, Space } from 'antd';
+import { Row, Col, Card, List, Typography, Space } from 'antd';
 import { useSelector } from 'react-redux';
 import { EditOutlined } from '@ant-design/icons';
 
@@ -21,23 +21,30 @@ const BuildingList = () => {
             <EditOutlined style={{ fontSize: '26px', cursor: 'pointer' }} />
           }
         >
-          <Row style={{ width: '100%' }} align="middle">
-            <Col span={12}>
-              <List.Item.Meta
-                title={building.name}
-                description={`${building.street}, ${building.number}, ${building.city}, ${building.country}`}
-              />
-            </Col>
-            <Col span={12}>
-              <Space>
-                <Text italic>
-                  Coordinates: lat: {building.coordinates.latitude.toFixed(3)},
-                  lng: {building.coordinates.longitude.toFixed(3)}
-                </Text>
-                <Text>Price: {building.price}</Text>
-              </Space>
-            </Col>
-          </Row>
+          <Card style={{ width: '100%', marginRight: '20px' }}>
+            <Row style={{ width: '100%' }} align="middle">
+              <Col span={12}>
+                <Card.Grid style={{ width: '80%' }}>
+                  <List.Item.Meta
+                    title={building.name}
+                    description={`${building.street}, ${building.number}, ${building.city}, ${building.country}`}
+                  />
+                </Card.Grid>
+              </Col>
+              <Col span={12}>
+                <Card.Grid style={{ width: '80%' }}>
+                  <Space>
+                    <Text italic>
+                      Coordinates: lat:{' '}
+                      {building.coordinates.latitude.toFixed(3)}, lng:{' '}
+                      {building.coordinates.longitude.toFixed(3)}
+                    </Text>
+                    <Text>Price: {building.price}</Text>
+                  </Space>
+                </Card.Grid>
+              </Col>
+            </Row>
+          </Card>
         </List.Item>
       )}
     />
